6d65ab936879d599963006b8e2e71507125ca486
Davert <davert.php@resend.cc>
2016-03-03 02:31:05 +0200

    updated test site

--- scripts/controllers/info.js
+++ scripts/controllers/info.js
@@ -1,4 +1,8 @@
 angular.module('testAppJsonApp')
   .controller('MainCtrl', function($scope) {
     $scope.description = "Welcome to event app";
+
+    window.setTimeout(function () {
+      document.getElementById('hello').textContent = 'Boom!';
+    }, 1000);
 });
